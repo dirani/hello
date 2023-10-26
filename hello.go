e6330b6608a210636385f1fd27cb39f43323a37d
Ricardo Dirani <dirani@gmail.com>
2023-10-26 16:26:27 -0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/dirani/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message, err := greetings.Hello("Gladys")
-	fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flog to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
 	if err != nil {
-		fmt.Println(greetings.Hello(""))
-	} else {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
+	fmt.Println(message)
 }
