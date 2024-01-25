4b03f84b1a8e60b0c5510b3ada7ef6ed61ec5d26
Ricardo Dirani <dirani@gmail.com>
2024-01-25 16:53:32 -0300

    Request greeting messages for the names

--- hello.go
+++ hello.go
@@ -4,22 +4,25 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/dirani/greetings"
+	"example.com/greetings"
 )
 
 func main() {
 	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flog to disable printing
+	// the log entry prefix and a flag to disable printing
 	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Gladys")
-	// If an error was returned, print it to the console and
-	// exit the program.
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
